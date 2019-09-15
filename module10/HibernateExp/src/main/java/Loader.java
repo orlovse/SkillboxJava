e7283937059cbc55f2897a311488daecbe725a24
orlovse <hajoff@gmail.com>
2019-09-15 20:29:43 +0300

    1

---
+++ module10/HibernateExp/src/main/java/Loader.java
@@ -0,0 +1,20 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Loader {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
+        Teachers teachers = session.get(Teachers.class, 1);
+        System.out.println("Имя учителя: " + teachers.getName() + ", возраст учителя: " + teachers.getAge());
+
+        sessionFactory.close();
+    }
+}
