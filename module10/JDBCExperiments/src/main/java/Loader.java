ff6016e409e50c519346d4cfc3e213d29b51866a
orlovse <hajoff@gmail.com>
2019-09-15 12:07:02 +0300

    1

---
+++ module10/JDBCExperiments/src/main/java/Loader.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class Loader {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/skillbox";
+        String user = "root";
+        String pass = "rootroot";
+
+        try {
+
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT name AS course_name, " +
+                            "students_count / (SELECT Month(max(subscription_date)) - Month(min(subscription_date)) FROM Subscriptions) " +
+                            "AS average_purchases_per_month FROM Courses"
+            );
+
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getString("course_name") + " - " +
+                                resultSet.getDouble("average_purchases_per_month")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
